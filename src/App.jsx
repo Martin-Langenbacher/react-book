8a2ec2a871a3503b6734c9a3a9c1c6822922cdf8
Martin Langenbacher <martin.langenbacher@telekom.de>
2021-03-24 17:25:29 +0100

    two cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
+//import React, { Component } from 'react';
 //import './App.css';
-import Card from './Card';
+//import Card from './Card';
+//import Animal from "./Animal";
+import Game from "./Game";
 
+export default function App() {
+    return <Game title="Supertrumpf" />
+}
+
+/*
 export default class App extends Component {
   render(){
+      const animal = new Animal('Elefant', 'placeholder.png', 3.3, 6000, 70, 1, 40);
     return (
         <div>
           <h1>Supertrumpf</h1>
-          <Card />
+            <Card animal={animal} unvovered />
         </div>
     );
   }
 }
 
+
+ */
 
 
 
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,21 +1,88 @@
 import * as React from 'react';
-import './Card.css';
+import PropTypes from 'prop-types';
 
-export default function Card() {
-    return (
+import './Card.css';
+import Animal from "./Animal";
+
+const uncovered = false;
+
+export default function Card({ animal, uncovered }) {
+    const front = (
         <div className="card">
-            <h1>Elefant</h1>
-            <img alt="Elefant" height="200" width="200"
-                 src={`${process.env.PUBLIC_URL}/placeholder.png`} />
-                 <table>
-                     <tbody>
-                        <tr><td>Größe</td><td>3.3 m</td></tr>
-                        <tr><td>Gewicht</td><td>6000 kg</td></tr>
-                        <tr><td>Alter</td><td>70 Jahre</td></tr>
-                        <tr><td>Nachkommen</td><td>1</td></tr>
-                        <tr><td>Geschwindigkeit</td><td>40 km</td></tr>
-                     </tbody>
-                 </table>
+            <h1> {animal.name ? animal.name : 'Unbekannt'}</h1>
+            {animal.image && (
+                <img alt={animal.name} src={`${process.env.PUBLIC_URL}/${animal.image}`}
+                     height="200" width="200" />
+            )}
+            <table>
+                <tbody>
+                {Object.keys(Animal.properties).map(property => {
+                    const animalProperty = Animal.properties[property];
+                    return (
+                        <tr key={property}>
+                            <td>{animalProperty.label}</td>
+                            <td>{animal[property]}&nbsp;{animalProperty.unit}</td>
+                        </tr>
+                    );
+                })}
+                </tbody>
+            </table>
         </div>
     );
+    const back = <div className="card back" />;
+    if (uncovered) {
+        return front;
+    } else {
+        return back;
+    }
+
+
+
 }
+
+
+Card.propTypes = {
+    uncovered: PropTypes.bool.isRequired,
+    animal: PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            image: PropTypes.string.isRequired,
+            size: PropTypes.number,
+            weight: PropTypes.number,
+            age: PropTypes.number,
+            offspring: PropTypes.number,
+            speed: PropTypes.number
+        }),
+};
+
+
+
+
+
+
+/*
+
+
+
+
+
+return (
+    <div className="card">
+
+    </div>
+);
+
+
+
+
+                     <!--
+                        <tr><td>Größe</td><td>{elephant.size}</td></tr>
+                        <tr><td>Gewicht</td><td>{elephant.weight}</td></tr>
+                        <tr><td>Alter</td><td>{elephant.age}</td></tr>
+                        <tr><td>Nachkommen</td><td>{elephant.offspring}</td></tr>
+                        <tr><td>Geschwindigkeit</td><td>{elephant.speed}</td></tr>
+                     -->
+
+            //}
+            //<img alt="Elefant" height="200" width="200"
+                 src={`${process.env.PUBLIC_URL}/placeholder.png`} />
+ */
